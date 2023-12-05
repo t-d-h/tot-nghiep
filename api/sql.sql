1f5373d60566e2863925f348ba1435edb44709f8
t-d-h <tdhoan526@gmail.com>
2023-12-05 16:53:28 +0700

    db data

--- api/sql.sql
+++ api/sql.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE databasename;
 
 CREATE TABLE users (
-    user_id varchar(255),
+    user_id int NOT NULL AUTO_INCREMENT,
     username varchar(255),
     password varchar(255),
     email varchar(255),
@@ -11,7 +11,7 @@ CREATE TABLE users (
 );
 
 CREATE TABLE servers (
-    server_id varchar(255),
+    server_id int NOT NULL AUTO_INCREMENT,
     serial varchar(255),
     cpu_cores int,
     memory_gb int,
@@ -20,3 +20,13 @@ CREATE TABLE servers (
     server_status varchar(255),
     ip_address varchar(255)
 );
+
+INSERT INTO users (username, password, email, role, team)
+VALUES (hoan1, hash1, email@gmail.com, sysadmin, infra);
+INSERT INTO users (username, password, email, role, team)
+VALUES (hoan2, hash2, email2@gmail.com, developer, backend);
+INSERT INTO users (username, password, email, role, team)
+VALUES (hoan3, hash3, email3@gmail.com, manager, bod);
+
+INSERT INTO servers (serial, cpu_cores, memory_gb, running_services, server_status, ip_address)
+VALUES (abc1234, 4, 8, product_api, in_use, 10.0.6.8);
